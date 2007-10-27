5f1ea6ce9388c9901908dc198c59326ad9723989
pdm <pdm>
2007-10-27 13:38:31 +0000

    XUL client: Don't try to focus the no longer existent element after startup

--- client/brailchem@brailcom.org/chrome/content/brailchem.js
+++ client/brailchem@brailcom.org/chrome/content/brailchem.js
@@ -24,7 +24,6 @@ function brailchem_start ()
 function brailchem_init ()
 {
     addEventListener ('focus', brailchem_focus_callback, true);
-    brailchem_focus (document.getElementById ('brailchem-primary'));
 }
 
 // Commands
